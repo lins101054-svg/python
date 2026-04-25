37ac4b7d152c639a4c6333bc66ea9fe7fb877c6c
lins101054-svg <lins101054@gmail.com>
2026-04-25 15:35:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-import requests
- 
+
 # 使用者輸入
 stock_no = input("請輸入股票代號（例如 2330）：")
-#date = input("請輸入查詢年月（格式：YYYYMMDD，例如 20260301）：")
+
  
+
 # API URL
 url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo={stock_no}"
+
  
+
 # 發送請求
-res = requests.get(url)
-data = res.json()
+res = requests.get(url) #原始
+data = res.json() #解析後的資料
+
  
+
 # 判斷是否成功
 if data["stat"] == "OK":
-    print( data["title"])
-    print( data["fields"])
-    print("資料：")
-    for row in data["data"]:
-        print(row)
+
+ 
+
+    print("前一天收盤價：",data["data"][-1][6]) #return render_template('stock.html', question=question, answer=answer)
 else:
-    print("查無資料，請確認股票代號或日期")
+    print("查無資料，請確認股票代號或日期") #return render_template('stock.html', question=question, answer=answer)
+
+
