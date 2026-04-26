1ae8f86a0c9e344529a75de8372afbac3faa9c45
lins101054-svg <lins101054@gmail.com>
2026-04-26 21:58:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -55,30 +55,3 @@ if __name__ == '__main__':
     # 開發用；部署用 gunicorn（見下方）
     app.run(host='0.0.0.0', debug=False)
 
-
-
-
-# 使用者輸入
-stock_no = input("請輸入股票代號（例如 2330）：")
-
- 
-
-# API URL
-url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo={stock_no}"
-
- 
-
-# 發送請求
-res = requests.get(url) #原始
-data = res.json() #解析後的資料
-
- 
-
-# 判斷是否成功
-if data["stat"] == "OK":
-
- 
-
-    print("前一天收盤價：",data["data"][-1][6]) #return render_template('stock.html', question=question, answer=answer)
-else:
-    print("查無資料，請確認股票代號或日期") #return render_template('stock.html', question=question, answer=answer)
