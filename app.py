c4e32756b7a27c23372ece061aec8a75221a11ca
lins101054-svg <lins101054@gmail.com>
2026-04-26 22:25:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,24 +19,6 @@ zh_ko_dict = {
     "愛": "사랑"
 }
 
-import requests #放在最前面? 函數stock內?
- 
-# 使用者輸入
-stock_no = input("請輸入股票代號（例如 2330）：")
- 
-# API URL
-url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo={stock_no}"
- 
-# 發送請求
-res = requests.get(url) #原始
-data = res.json() #解析後的資料
- 
-# 判斷是否成功
-if data["stat"] == "OK":
- 
-    print("前一天收盤價：",data["data"][-1][6]) #return render_template('stock.html', question=question, answer=answer)
-else:
-    print("查無資料，請確認股票代號或日期") #return render_template('stock.html', question=question, answer=answer)
     
 # homepage process
 @app.route('/')
@@ -67,6 +49,25 @@ def stock():
         return render_template('stock.html', question=question, answer=answer)
     # GET 時給空白欄位
     return render_template('stock.html', question="", answer="")
+
+import requests #放在最前面? 函數stock內?
+ 
+# 使用者輸入
+stock_no = input("請輸入股票代號（例如 2330）：")
+ 
+# API URL
+url = f"https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&stockNo={stock_no}"
+ 
+# 發送請求
+res = requests.get(url) #原始
+data = res.json() #解析後的資料
+ 
+# 判斷是否成功
+if data["stat"] == "OK":
+ 
+    print("前一天收盤價：",data["data"][-1][6]) #return render_template('stock.html', question=question, answer=answer)
+else:
+    print("查無資料，請確認股票代號或日期") #return render_template('stock.html', question=question, answer=answer)
 
 if __name__ == '__main__':
     # 開發用；部署用 gunicorn（見下方）
